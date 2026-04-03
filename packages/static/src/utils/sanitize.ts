b29da0d2e0daf5a1e8c7b8aeb6de46c462e1b4be
dwlhm <dwiilhamm026@gmail.com>
2026-04-03 23:59:43 +0700

    refactor: add JSDoc documentation and improve variable naming in sanitizeAttrValue

--- packages/static/src/utils/sanitize.ts
+++ packages/static/src/utils/sanitize.ts
@@ -1,8 +1,14 @@
+/**
+ * Sanitize a string for use as an HTML attribute value.
+ * Replaces special characters with their HTML entities.
+ * @param val The string to sanitize.
+ * @returns The sanitized string.
+ */
 export const sanitizeAttrValue = (val: string): string =>
-    val.replace(/["'<>&]/g, (c) => ({
+    val.replace(/["'<>&]/g, (char) => ({
         '"': '&quot;',
         '&': '&amp;',
         "'": '&#39;',
         '<': '&lt;',
         '>': '&gt;',
-    }[c] ?? c))
+    }[char] ?? char))
